5ee4a5f74f1ae5008389a72db24c74436702174a
Rory MacLeod <mail@rorymacleod.com>
2018-04-15 20:27:07 +0100

    WIP HttpClientProvider (11/12)

---
+++ Toolbox.UnitTests/Net/Http/HttpClientProviderTests.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.Caching;
+using System.Threading;
+using NUnit.Framework;
+using Toolbox.Net.Http;
+
+namespace Toolbox.UnitTests.Net.Http
+{
+    [TestFixture]
+    public class HttpClientProviderTests
+    {
+        [TestFixture]
+        public class GetHttpClient
+        {
+            [Test]
+            public void ReturnsInstanceWithSpecifiedBaseUri()
+            {
+                var provider = new HttpClientProvider();
+                var client = provider.GetHttpClient("http://alpha.com");
+
+                Assert.That(client, Is.Not.Null);
+                Assert.That(client.BaseAddress.Equals(new Uri("http://alpha.com")));
+
+                var uri = new Uri("http://bravo.net");
+                Assert.That(provider.GetHttpClient(uri).BaseAddress, Is.SameAs(uri));
+            }
+
+            [Test]
+            public void ReturnsSameInstanceForSameUri()
+            {
+                var provider = new HttpClientProvider();
+                var client1 = provider.GetHttpClient("http://alpha.com");
+                var client2 = provider.GetHttpClient(new Uri("http://ALPHA.com"));
+
+                Assert.That(client1, Is.SameAs(client2));
+            }
+
+            [Test]
+            public void ReturnsSameInstanceIgnoringUriPath()
+            {
+                var provider = new HttpClientProvider();
+                var client1 = provider.GetHttpClient("http://alpha.com/bravo.cshtml");
+                var client2 = provider.GetHttpClient(new Uri("http://alpha.com/charlie.html"));
+
+                Assert.That(client1, Is.SameAs(client2));
+            }
+
+            [Test]
+            public void ReturnsSameInstanceIgnoringUriQueryString()
+            {
+                var provider = new HttpClientProvider();
+                var client1 = provider.GetHttpClient("http://alpha.com/bravo?c=charlie");
+                var client2 = provider.GetHttpClient(new Uri("http://alpha.com/bravo/?c=delta"));
+
+                Assert.That(client1, Is.SameAs(client2));
+            }
+
+            [Test]
+            public void ThrowsIfUriIsRelative()
+            {
+                var provider = new HttpClientProvider();
+                try
+                {
+                    provider.GetHttpClient(new Uri("/bravo?c=charlie", UriKind.Relative));
+                }
+                catch (ArgumentException ex)
+                {
+                    Assert.That(ex.Message, Does.Contain("must be absolute"));
+                    return;
+                }
+
+                Assert.Fail("The expected exception was not thrown.");
+            }
+
+            [Test]
+            public void UsesSpecifiedCacheObject()
+            {
+                var cache = new MemoryCache("alpha");
+                var provider = new HttpClientProvider(cache);
+                provider.GetHttpClient("https://bravo.com");
+
+                Assert.That(cache.GetCount(), Is.EqualTo(1));
+            }
+            
+            [Test]
+            public void ReturnsNewInstanceForDifferentUri()
+            {
+                var provider = new HttpClientProvider();
+                var client1 = provider.GetHttpClient("http://alpha.com");
+                var client2 = provider.GetHttpClient(new Uri("http://bravo.net"));
+
+                Assert.That(client1, Is.Not.SameAs(client2));
+            }
+
+            [Test]
+            public void ReturnsNewInstanceForDifferentScheme()
+            {
+                var provider = new HttpClientProvider();
+                var client1 = provider.GetHttpClient("http://alpha.com");
+                var client2 = provider.GetHttpClient("https://alpha.com");
+
+                Assert.That(client1, Is.Not.SameAs(client2));
+            }
+
+            [Test]
+            public void ReturnsNewInstanceForDifferentPort()
+            {
+                var provider = new HttpClientProvider();
+                var client1 = provider.GetHttpClient("http://alpha.com");
+                var client2 = provider.GetHttpClient("http://alpha.com:8080");
+
+                Assert.That(client1, Is.Not.SameAs(client2));
+            }
+
+            [Test]
+            public void CallsInitializerForNewInstance()
+            {
+                var provider = new HttpClientProvider();
+                var calls = new List<HttpClient>();
+                var client = provider.GetHttpClient("http://alpha", h => calls.Add(h));
+
+                Assert.That(calls.Count, Is.EqualTo(1));
+                Assert.That(calls[0], Is.SameAs(client));
+            }
+
+            [Test]
+            public void ReturnsNewInstanceIfTimeToLiveHasExpired()
+            {
+                var provider = new HttpClientProvider {
+                    TimeToLive = new TimeSpan(0, 0, 0, 0, 100)
+                };
+                var client1 = provider.GetHttpClient("http://alpha");
+                Thread.Sleep(150);
+                var client2 = provider.GetHttpClient("http://alpha");
+
+                Assert.That(client1, Is.Not.SameAs(client2));
+            }
+
+            [Test]
+            public void ReturnsNewInstanceAfterUriHasBeenInvalidated()
+            {
+                Assert.Ignore("The test has not been implemented.");
+            }
+        }
+
+        [TestFixture]
+        public class Invalidate
+        {
+
+        }
+
+        [TestFixture]
+        public class InvalidateAll
+        {
+
+        }
+    }
+}
---
+++ Toolbox/Net/Http/HttpClientProvider.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Caching;
+
+namespace Toolbox.Net.Http
+{
+    /// <summary>
+    /// Represents a source of <see cref="HttpClient"/> instances.
+    /// </summary>
+    public class HttpClientProvider : IHttpClientProvider
+    {
+        /// <summary>
+        /// Cache of <see cref="HttpClient"/> instances.
+        /// </summary>
+        private readonly ObjectCache Cache;
+
+        /// <summary>
+        /// Synchronizes access to the cache by multiple threads. Note that thread-safety can only be guaranteed when
+        /// <c>HttpClientProvider</c> creates its own cache, or when the supplied cache is not used by any other code.
+        /// </summary>
+        private readonly object SyncRoot = new object();
+
+        /// <summary>
+        /// Specifies the maximum lifetime for any cached <see cref="HttpClient"/> instance.
+        /// </summary>
+        public TimeSpan TimeToLive { get; set; }
+
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpClientProvider"/> class to use a private, in-memory, cache.
+        /// </summary>
+        public HttpClientProvider() : this(new MemoryCache("HttpClientProvider"))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpClientProvider"/> class using the specified cache.
+        /// </summary>
+        /// <param name="cache">An <see cref="ObjectCache"/> instance to hold the <see cref="HttpClient"/> 
+        /// instances.</param>
+        public HttpClientProvider(ObjectCache cache)
+        {
+            Cache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+
+
+        /// <summary>
+        /// Gets an <see cref="HttpClient"/> object that can be used to access resources on the specified server.
+        /// </summary>
+        /// <param name="baseUri">The URI of the server to access.</param>
+        /// <returns>An <see cref="HttpClient"/> object for the specified URI. Note that this may be a cached or 
+        /// singleton instance - changing any properties on the <c>HttpClient</c> will affect all usages in the 
+        /// app-domain.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="baseUri"/> is a null-reference or empty 
+        /// string.</exception>
+        /// <exception cref="UriFormatException"><paramref name="baseUri"/> cannot be parsed by 
+        /// <see cref="Uri"/>.</exception>
+        public HttpClient GetHttpClient(string baseUri)
+        {
+            return GetHttpClient(baseUri, null);
+        }
+
+        /// <summary>
+        /// Gets an <see cref="HttpClient"/> object that can be used to access resources on the specified server.
+        /// </summary>
+        /// <param name="baseUri">The URI of the server to access.</param>
+        /// <param name="initializer">An optional <see cref="Action{T}"/> called to initialize a new 
+        /// <see cref="HttpClient"/>, if one is created. Note that any other calls to <c>GetHttpClient</c> will be 
+        /// blocked while the initializer is executing.</param>
+        /// <returns>An <see cref="HttpClient"/> object for the specified URI. Note that this may be a cached or 
+        /// singleton instance - changing any properties on the <c>HttpClient</c> will affect all usages in the 
+        /// app-domain.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="baseUri"/> is a null-reference or empty 
+        /// string.</exception>
+        /// <exception cref="UriFormatException"><paramref name="baseUri"/> cannot be parsed by 
+        /// <see cref="Uri"/>.</exception>
+        public HttpClient GetHttpClient(string baseUri, Action<HttpClient> initializer)
+        {
+            if (string.IsNullOrEmpty(baseUri))
+                throw new ArgumentNullException(nameof(baseUri));
+
+            return GetHttpClient(new Uri(baseUri, UriKind.Absolute), initializer);
+        }
+
+        /// <summary>
+        /// Gets an <see cref="HttpClient"/> object that can be used to access resources on the specified server.
+        /// </summary>
+        /// <param name="baseUri">The URI of the server to access.</param>
+        /// <returns>An <see cref="HttpClient"/> object for the specified URI. Note that this may be a cached or 
+        /// singleton instance - changing any properties on the <c>HttpClient</c> will affect all usages in the 
+        /// app-domain.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="baseUri"/> is a null-reference.</exception>
+        /// <exception cref="ArgumentException"><paramref name="baseUri"/> is not an absolute URI.</exception>
+        public HttpClient GetHttpClient(Uri baseUri)
+        {
+            return GetHttpClient(baseUri, null);
+        }
+
+        /// <summary>
+        /// Gets an <see cref="HttpClient"/> object that can be used to access resources on the specified server.
+        /// </summary>
+        /// <param name="baseUri">The URI of the server to access.</param>
+        /// <returns>An <see cref="HttpClient"/> object for the specified URI. Note that this may be a cached or 
+        /// singleton instance - changing any properties on the <c>HttpClient</c> will affect all usages in the 
+        /// app-domain.</returns>
+        /// <param name="initializer">An optional <see cref="Action{T}"/> called to initialize a new 
+        /// <see cref="HttpClient"/>, if one is created. Note that any other calls to <c>GetHttpClient</c> will be 
+        /// blocked while the initializer is executing.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="baseUri"/> is a null-reference.</exception>
+        /// <exception cref="ArgumentException"><paramref name="baseUri"/> is not an absolute URI.</exception>
+        public HttpClient GetHttpClient(Uri baseUri, Action<HttpClient> initializer)
+        {
+            if (baseUri == null)
+                throw new ArgumentNullException(nameof(baseUri));
+            if (!baseUri.IsAbsoluteUri)
+                throw new ArgumentException("The URI must be absolute.", nameof(baseUri));
+
+            var key = baseUri.GetLeftPart(UriPartial.Authority).ToLowerInvariant();
+            CacheItem item;
+            lock (SyncRoot)
+            {
+                item = Cache.GetCacheItem(key);
+                if (item == null)
+                {
+                    var client = new HttpClient {
+                        BaseAddress = baseUri
+                    };
+                    initializer?.Invoke(client);
+                    item = new CacheItem(key, client);
+                    var policy = new CacheItemPolicy();
+                    var ttl = TimeToLive;
+                    if (ttl > TimeSpan.Zero)
+                    {
+                        policy.AbsoluteExpiration = DateTimeOffset.UtcNow.Add(TimeToLive);
+                    }
+                    Cache.Set(item, policy);
+                }
+            }
+            return (HttpClient)item.Value;
+        }
+
+        public void Invalidate(string baseUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Invalidate(Uri baseUri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

049a90a2c8feb13530886db11c457013ee410945
Rory MacLeod <mail@rorymacleod.com>
2021-09-07 12:35:26 +0100

    Migrate StringExtension methods to .NET 6 project.

---
+++ Toolbox.Tests/StringExtensionsTests.cs
@@ -0,0 +1,104 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Toolbox.Tests;
+
+public class StringExtensionsTests
+{
+    public class ToHash
+    {
+        [Theory]
+        [InlineData("Alpha", "58061aa544398a798e33181a443b15b7746fab16")]
+        [InlineData("Alpha1", "2afd643c8465abbf311988fc141204ec1e462c49")]
+        [InlineData("Bravo", "f8b83dde53d52771dda7582ed1b5f5928dec9b2f")]
+        [InlineData("", "da39a3ee5e6b4b0d3255bfef95601890afd80709")]
+        public void Returns_SHA1_hash(string str, string expected)
+        {
+            string hash = str.ToHash();
+
+            hash.Should().Be(expected);
+        }
+    }
+
+    public class Truncate
+    {
+        [Theory]
+        [InlineData("alpha", 10, "alpha")]
+        [InlineData("bravo", 5, "bravo")]
+        [InlineData("charlie", 4, "char")]
+        [InlineData("delta", 0, "")]
+        public void Returns_shortened_string(string str, int length, string expected)
+        {
+            str.Truncate(length).Should().Be(expected);
+        }
+
+        [Fact]
+        public void Throws_if_length_is_less_than_zero()
+        {
+            try
+            {
+                "Alpha".Truncate(-1);
+            }
+            catch (ArgumentException ex)
+            {
+                ex.ParamName.Should().Be("length");
+                return;
+            }
+                
+            Assert.True(false, "Expected exception was not thrown.");
+        }
+
+        [Theory]
+        [InlineData("Alpha", 10, '…', "Alpha")]
+        [InlineData("Bravo", 5, 'x', "Bravo")]
+        [InlineData("Charlie", 4, '_', "Cha_")]
+        [InlineData("Delta", 1, 'X', "X")]
+        public void Returns_shortened_string_with_char_suffix(string str, int length, char suffix, string expected)
+        {
+            str.Truncate(length, suffix).Should().Be(expected);
+        }
+
+        [Fact]
+        public void Throws_if_length_with_suffix_is_less_than_one()
+        {
+            try
+            {
+                "Alpha".Truncate(0, '-');
+            }
+            catch (ArgumentException ex)
+            {
+                ex.ParamName.Should().Be("length");
+                return;
+            }
+                
+            Assert.True(false, "Exception was not thrown");
+        }
+
+        [Theory]
+        [InlineData("Alpha", 10, "XXX", "Alpha")]
+        [InlineData("Bravo", 5, "Y", "Bravo")]
+        [InlineData("Charlie", 4, "...", "C...")]
+        [InlineData("Delta", 2, "12", "12")]
+        public void Returns_shortened_string_with_string_tail(string str, int length, string tail, string expected)
+        {
+            str.Truncate(length, tail).Should().Be(expected);
+        }
+
+        [Fact]
+        public void Throws_if_length_is_less_than_tail()
+        {
+            try
+            {
+                "Alpha".Truncate(2, "...");
+            }
+            catch (ArgumentException ex)
+            {
+                ex.ParamName.Should().Be("length");
+                return;
+            }
+                
+            Assert.True(false, "Expected exception was not thrown.");
+        }
+    }
+}
---
+++ Toolbox/StringExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System;
+
+namespace Toolbox;
+
+public static class StringExtensions
+{
+    public const char Ellipsis = '…';
+
+    /// <summary>
+    /// Returns the SHA1 hash of the string.
+    /// </summary>
+    public static string ToHash(this string str)
+    {
+        using var crypto = HashAlgorithm.Create("SHA1") ??
+            throw new InvalidOperationException("Hash algorithm for \"SHA1\" not found.");
+
+        var hash = crypto.ComputeHash(Encoding.UTF8.GetBytes(str));
+        var result = new StringBuilder(hash.Length * 2);
+        foreach (var b in hash)
+        {
+            result.Append(b.ToString("x2"));
+        }
+
+        return result.ToString();
+    }
+
+    /// <summary>
+    /// Removes one occurance of the specified string if it appears at the end of the current string.
+    /// </summary>
+    public static string TrimEnd(this string current, string toRemove)
+    {
+        return current.TrimEnd(toRemove, StringComparison.CurrentCulture);
+    }
+
+    /// <summary>
+    /// Removes one occurance of the specified string if it appears at the end of the current string.
+    /// </summary>
+    public static string TrimEnd(this string current, string toRemove, StringComparison comparisonType)
+    {
+        if (current.EndsWith(toRemove, comparisonType))
+        {
+            return current.Substring(0, current.Length - toRemove.Length);
+        }
+
+        return current;
+    }
+
+    /// <summary>
+    /// Returns the string shortened to the given length, including the optional suffix.
+    /// </summary>
+    public static string Truncate(this string str, int length, char? suffix = null)
+    {
+        if (length < 0)
+            throw new ArgumentException("Must not be less than 0.", nameof(length));
+        if (suffix.HasValue && length < 1)
+            throw new ArgumentException("Must not be less than 1.", nameof(length));
+
+        if (length >= str.Length)
+            return str;
+
+        return suffix.HasValue ? str.Substring(0, length - 1) + suffix.Value : str.Substring(0, length);
+    }
+
+    /// <summary>
+    /// Returns the string shortened to the given length, including the optional suffix.
+    /// </summary>
+    public static string Truncate(this string str, int length, string tail)
+    {
+        if (length < 0)
+            throw new ArgumentException("Must not be less than 0.", nameof(length));
+        if (tail != null && length < tail.Length)
+            throw new ArgumentException($"'{nameof(length)}' must not be less than the length of '{nameof(tail)}'.",
+                nameof(length));
+
+        if (length >= str.Length)
+            return str;
+
+        return tail != null ? string.Concat(str.AsSpan(0, length - tail.Length), tail) : str.Substring(0, length);
+    }
+}

3ae73a8ab9bdd14e94f7e4898940e1fd608e8794
Rory MacLeod <mail@rorymacleod.com>
2022-07-16 13:16:21 +0100

    Rework exception message handling to be simpler.
    Code has been refactored into a single ExceptionExtensions class,
    making it easier to add to an existing application (it's just one
    file). Also makes AppException redundant.

---
+++ Toolbox.Tests/ErrorHandling/ExceptionInspectionTests.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using FluentAssertions;
+using Toolbox.ErrorHandling;
+using Xunit;
+
+namespace Toolbox.Tests.ErrorHandling;
+
+public class ExceptionInspectionTests
+{
+    private class TestException : Exception
+    {
+        public TestException() { }
+        public TestException(string message) : base(message) { }
+        public TestException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
+    private static Exception CreateException()
+    {
+        return new InvalidOperationException("Alpha.",
+            new TestException("Bravo.", new Exception("Charlie."))
+                .SetData("Id", 100)
+                .SetData("Name", "bravo"));
+    }
+
+    [Fact]
+    public void Get_single_line_message_from_nested_exceptions()
+    {
+        string message = CreateException().GetAllMessages();
+        message.Should().Be("Alpha. Bravo. Charlie.");
+    }
+
+    [Fact]
+    public void Get_single_line_message_with_data()
+    {
+        string message = CreateException().GetAllMessages(includeData: true);
+        message.Should().Be("Alpha. Bravo (Id=[100], Name=[bravo]). Charlie.");
+    }
+
+    [Fact]
+    public void Renders_null_values_in_data()
+    {
+        var exception = new TestException("Alpha").SetData("Id", null);
+        string message = exception.GetAllMessages(includeData: true);
+        message.Should().Be("Alpha (Id=[<null>]).");
+    }
+
+    [Fact]
+    public void Get_multiline_message_from_nested_exceptions()
+    {
+        var expected = new StringBuilder();
+        expected.AppendLine("(InvalidOperation) Alpha.");
+        expected.AppendLine("(Test) Bravo.");
+        expected.Append("(Exception) Charlie.");
+
+        var message = CreateException().GetAllMessages(true);
+        message.Should().Be(expected.ToString());
+    }
+
+    [Fact]
+    public void Get_multiline_message_with_data()
+    {
+        var expected = new StringBuilder();
+        expected.AppendLine("(InvalidOperation) Alpha.");
+        expected.AppendLine("(Test) Bravo.");
+        expected.AppendLine("- Id=[100]");
+        expected.AppendLine("- Name=[bravo]");
+        expected.Append("(Exception) Charlie.");
+
+        var message = CreateException().GetAllMessages(true, true);
+        message.Should().Be(expected.ToString());
+    }
+
+    [Fact]
+    public void Gets_multiline_message_with_aggregate_exception()
+    {
+        var expected = new StringBuilder();
+        expected.AppendLine("(InvalidOperation) Alpha.");
+        expected.AppendLine("2 errors occurred in parallel:");
+        expected.AppendLine(" 1| (Test) Bravo.");
+        expected.AppendLine("    (Exception) Charlie.");
+        expected.Append(" 2| (Test) Delta.");
+
+        var exception = new InvalidOperationException("Alpha.",
+            new AggregateException(
+                new TestException("Bravo.", new Exception("Charlie.")),
+                new TestException("Delta.")
+            ));
+        var message = exception.GetAllMessages(true, true);
+        message.Should().Be(expected.ToString());
+    }
+
+    [Fact]
+    public void Gets_multiline_message_with_aggregate_exception_and_data()
+    {
+        var expected = new StringBuilder();
+        expected.AppendLine("(InvalidOperation) Alpha.");
+        expected.AppendLine("2 errors occurred in parallel:");
+        expected.AppendLine(" 1| (Test) Bravo.");
+        expected.AppendLine("    - Id=[100]");
+        expected.AppendLine("    - Name=[bravo]");
+        expected.AppendLine("    (Exception) Charlie.");
+        expected.AppendLine(" 2| (Test) Delta.");
+        expected.Append("    - Id=[200]");
+
+        var exception = new InvalidOperationException("Alpha.",
+            new AggregateException(
+                new TestException("Bravo.", new Exception("Charlie."))
+                    .SetData("Id", 100)
+                    .SetData("Name", "bravo"),
+                new TestException("Delta.").SetData("Id", 200)
+            ));
+        var message = exception.GetAllMessages(true, true);
+        message.Should().Be(expected.ToString());
+    }
+
+    [Fact]
+    public void Handles_nested_aggregate_exceptions()
+    {
+        var expected = new StringBuilder();
+        expected.AppendLine("(Test) Alpha.");
+        expected.AppendLine("3 errors occurred in parallel:");
+        expected.AppendLine(" 1| (Test) Bravo.");
+        expected.AppendLine("    (Exception) Charlie.");
+        expected.AppendLine(" 2| 2 errors occurred in parallel:");
+        expected.AppendLine("     1| (Test) Delta.");
+        expected.AppendLine("        - Id=[200]");
+        expected.AppendLine("     2| (Exception) Echo.");
+        expected.Append(" 3| (Exception) Foxtrot.");
+
+        var exception = new TestException("Alpha.",
+            new AggregateException(
+                new TestException("Bravo.", new Exception("Charlie.")),
+                new AggregateException(
+                    new TestException("Delta.").SetData("Id", 200),
+                    new Exception("Echo.")),
+                new Exception("Foxtrot.")));
+
+        var message = exception.GetAllMessages(true, true);
+        message.Should().Be(expected.ToString());
+    }
+
+    [Fact]
+    public void Hides_insignificant_exceptions()
+    {
+        var exception = new TestException("Alpha.",
+            new TargetInvocationException("Bravo.",
+                new Exception("Charlie.")));
+
+        var message = exception.GetAllMessages();
+        message.Should().Be("Alpha. Charlie.");
+    }
+
+    [Fact]
+    public void Hides_aggregate_exception_with_one_exception()
+    {
+        var exception = new AggregateException("Alpha.", new TestException("Bravo."));
+
+        var message = exception.GetAllMessages();
+        message.Should().Be("Bravo.");
+    }
+
+    [Fact]
+    public void Gets_message_when_aggregate_exception_has_zero_exceptions()
+    {
+        var exception = new AggregateException("Alpha.");
+
+        var message = exception.GetAllMessages();
+        message.Should().Be("Alpha.");
+    }
+}
--- Toolbox/ErrorHandling/ExceptionExtensions.cs
+++ Toolbox/ErrorHandling/ExceptionExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using Toolbox.Collections;
 
 namespace Toolbox.ErrorHandling;
 
@@ -9,50 +12,11 @@ namespace Toolbox.ErrorHandling;
 /// </summary>
 public static class ExceptionExtensions
 {
-    private static void AppendParallelMessages(ExceptionFormatting formatting, StringBuilder sb, AggregateException aggEx)
-    {
-        if (formatting == ExceptionFormatting.SingleLine)
-        {
-            // Example: "2 errors occurred in parallel: (1) Alpha. (2) Bravo. Charlie."
-            sb.Append($"{aggEx.InnerExceptions.Count} errors occurred in parallel: ");
-        }
-        else
-        {
-            // Example:
-            // 2 errors occurred in parallel:
-            // (1) Alpha.
-            //     - Data
-            // (2) Bravo.
-            //     Charlie.
-            //     - Data
-            sb.AppendLine($"{aggEx.InnerExceptions.Count} errors occurred in parallel:");
-        }
-
-        for (int exNum = 1; exNum <= aggEx.InnerExceptions.Count; exNum++)
-        {
-            var innerEx = aggEx.InnerExceptions[exNum - 1];
-            var innerMsg = GetAllMessages(innerEx, formatting);
-
-            if (formatting == ExceptionFormatting.SingleLine)
-            {
-                sb.Append($" ({exNum}) {innerMsg}");
-            }
-            else
-            {
-                sb.AppendLine($"{exNum,2}| " +
-                    innerMsg.Replace(Environment.NewLine, Environment.NewLine + "    "));
-            }
-        }
-    }
-
     /// <summary>
     /// Returns the exception and all of the exceptions in the inner-exception hierarchy as a single flat collection.
     /// </summary>
     public static IEnumerable<Exception> Flatten(this Exception exception)
     {
-        if (exception == null)
-            throw new ArgumentNullException(nameof(exception));
-
         yield return exception;
 
         if (exception is AggregateException aggEx)
@@ -78,28 +42,27 @@ public static class ExceptionExtensions
     /// Gets all error messages from the significant exceptions reachable from the given exception combined into a
     /// single message.
     /// </summary>
-    public static string GetAllMessages(this Exception exception,
-        ExceptionFormatting formatting = ExceptionFormatting.SingleLine)
+    public static string GetAllMessages(this Exception exception, bool multiline = false, bool includeData = false)
     {
         var sb = new StringBuilder();
         foreach (var ex in exception.Flatten())
         {
             if (ex is AggregateException aggEx && aggEx.InnerExceptions.Count > 1)
             {
-                AppendParallelMessages(formatting, sb, aggEx);
+                AppendParallelMessages(sb, aggEx, multiline, includeData);
                 break;
             }
 
-            if (IsSignificant(ex))
+            if (IsSignificant(ex) || (ex.InnerException == null && sb.Length == 0))
             {
-                sb.Append(GetMessage(ex, formatting));
-                if (formatting == ExceptionFormatting.SingleLine)
+                sb.Append(GetMessage(ex, multiline, includeData));
+                if (multiline)
                 {
-                    sb.Append(' ');
+                    sb.AppendLine();
                 }
                 else
                 {
-                    sb.AppendLine();
+                    sb.Append(' ');
                 }
             }
         }
@@ -111,26 +74,40 @@ public static class ExceptionExtensions
     /// <summary>
     /// Returns a dictionary containing data from the exception that can be used to build a detailed error message.
     /// </summary>
-    public static SortedDictionary<string, string> GetData(this Exception exception)
+    public static SortedDictionary<string, string?> GetData(this Exception exception)
     {
-        var data = new SortedDictionary<string, string>();
-        ExceptionInspector.Get(typeof(Exception)).GetData(exception, data);
+        var data = new SortedDictionary<string, string?>();
+        foreach (string key in exception.Data.Keys)
+        {
+            object? value = exception.Data[key];
+            data[key] = value == null ? null : Convert.ToString(value);
+        }
+
         return data;
     }
 
     /// <summary>
     /// Gets a detailed error message built from the exception's own message and its properties.
     /// </summary>
-    public static string GetMessage(this Exception exception,
-        ExceptionFormatting formatting = ExceptionFormatting.SingleLine)
+    public static string GetMessage(this Exception exception, bool multiline = false, bool includeData = false)
     {
-        var inspector = ExceptionInspector.Get(exception.GetType());
-        if (formatting == ExceptionFormatting.Detailed)
+        var sb = new StringBuilder();
+        if (multiline)
         {
-            return inspector.GetMessage(exception, GetData(exception), formatting);
+            AppendExceptionName(sb, exception);
+            AppendMessage(sb, exception);
+        }
+        else
+        {
+            AppendMessage(sb, exception);
         }
 
-        return inspector.GetMessage(exception, null, formatting);
+        if (includeData)
+        {
+            AppendAllData(sb, exception, multiline);
+        }
+
+        return sb.ToString();
     }
 
     /// <summary>
@@ -151,7 +128,15 @@ public static class ExceptionExtensions
     /// </summary>
     public static bool IsSignificant(this Exception exception)
     {
-        return ExceptionInspector.Get(exception.GetType()).IsSignificant(exception);
+        switch (exception.GetType().FullName)
+        {
+            case "System.AggregateException":
+            case "System.Reflection.TargetInvocationException":
+                return false;
+
+            default:
+                return true;
+        }
     }
 
     /// <summary>
@@ -160,6 +145,138 @@ public static class ExceptionExtensions
     /// </summary>
     public static bool IsTransient(this Exception exception)
     {
-        return ExceptionInspector.Get(exception.GetType()).IsTransient(exception);
+        switch (exception.GetType().FullName)
+        {
+            case "System.Threading.ThreadAbortException":
+            case "System.OperationCancelledException":
+            case "System.Threading.Tasks.TaskCanceledException":
+            case "System.TimeoutException":
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    /// <summary>
+    /// Populates the exception's <see cref="Exception.Data"/> dictionary from the given dictionary.
+    /// </summary>
+    public static T SetData<T>(this T exception, IDictionary data) where T : Exception
+    {
+        foreach (var key in data.Keys)
+        {
+            exception.Data[key] = data[key];
+        }
+
+        return exception;
+    }
+
+    /// <summary>
+    /// Populates the exception's <see cref="Exception.Data"/> dictionary from the public properties of the given object.
+    /// </summary>
+    public static T SetData<T>(this T exception, object? data) where T : Exception
+    {
+        DictionaryHelper.AddObjectTo(exception.Data, data);
+        return exception;
+    }
+
+    /// <summary>
+    /// Adds the given key-value pair to the exception's <see cref="Exception.Data"/> dictionary.
+    /// </summary>
+    public static T SetData<T>(this T exception, string key, object? value) where T : Exception
+    {
+        exception.Data[key] = value;
+        return exception;
+    }
+
+    private static void AppendAllData(StringBuilder sb, Exception exception, bool multiline)
+    {
+        var data = exception.GetData();
+        if (data.Count == 0)
+            return;
+
+        if (multiline)
+        {
+            sb.AppendLine();
+        }
+        else
+        {
+            if (sb[^1] == '.')
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+            sb.Append(" (");
+        }
+
+        int i = 0;
+        foreach (var (key, value) in data)
+        {
+            sb.Append(multiline ? $"- {key}" : key);
+            sb.Append($"=[{value ?? "<null>"}]");
+            if (++i < data.Count)
+            {
+                if (multiline) {
+                    sb.AppendLine();
+                }
+                else
+                {
+                    sb.Append(", ");
+                }
+            }
+        }
+
+        if (!multiline)
+        {
+            sb.Append(").");
+        }
+    }
+
+    private static void AppendExceptionName(StringBuilder sb, Exception exception)
+    {
+        var exType = exception.GetType();
+        var name = exType == typeof(Exception) ? exType.Name : exType.Name.TrimEnd("Exception",
+            StringComparison.InvariantCultureIgnoreCase);
+        sb.AppendFormat("({0}) ", name);
+    }
+
+    private static void AppendMessage(StringBuilder sb, Exception exception)
+    {
+        sb.Append(exception.Message);
+    }
+
+    private static void AppendParallelMessages(StringBuilder sb, AggregateException aggEx, bool multiline, bool includeData)
+    {
+        if (multiline)
+        {
+            // Example:
+            // 2 errors occurred in parallel:
+            //  1| Alpha.
+            //     - Data
+            //  2| Bravo.
+            //     Charlie.
+            //     - Data
+            sb.AppendLine($"{aggEx.InnerExceptions.Count} errors occurred in parallel:");
+        }
+        else
+        {
+            // Example: "2 errors occurred in parallel: (1) Alpha. (2) Bravo. Charlie."
+            sb.Append($"{aggEx.InnerExceptions.Count} errors occurred in parallel: ");
+        }
+
+        for (int exNum = 1; exNum <= aggEx.InnerExceptions.Count; exNum++)
+        {
+            var innerEx = aggEx.InnerExceptions[exNum - 1];
+            var innerMsg = GetAllMessages(innerEx, multiline, includeData);
+
+            if (multiline)
+            {
+                sb.AppendLine($"{exNum,2}| " +
+                    innerMsg.Replace(Environment.NewLine, Environment.NewLine + "    "));
+            }
+            else
+            {
+                sb.Append($" ({exNum}) {innerMsg}");
+            }
+        }
     }
 }
